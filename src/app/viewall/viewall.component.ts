e2452624a195aeb615ce6776e30b31b30986e6b8
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-25 10:59:06 +0530

    data

--- src/app/viewall/viewall.component.ts
+++ src/app/viewall/viewall.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./viewall.component.css']
 })
 export class ViewallComponent {
+  data:any = [
+    {
+    "Name":"Jitty",
+    "Designation":"Software Engineer",
+    "Salary":50000,
+    "Company":"Nest",
+    "Email":"Jitty23@gmail.com"
+  },
+  {
+    "Name":"Arjun",
+    "Designation":"Software Engineer",
+    "Salary":50000,
+    "Company":"Nest",
+    "Email":"Arjun@gmail.com" 
+  },
+  {
+    "Name":"Raju",
+    "Designation":"Software Engineer",
+    "Salary":50000,
+    "Company":"Nest",
+    "Email":"Raju23@gmail.com"
+  },
+  {
+    "Name":"Sam",
+    "Designation":"Software Engineer",
+    "Salary":50000,
+    "Company":"Nest",
+    "Email":"Jitty23@gmail.com"
+  },
+  {
+    "Name":"Ram",
+    "Designation":"Software Engineer",
+    "Salary":50000,
+    "Company":"Nest",
+    "Email":"Ram23@gmail.com"
+  }
 
+  ]
 }
+
+ 
+
